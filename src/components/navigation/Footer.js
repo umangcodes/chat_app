c69459fad296b0ae41dae887320e4997dfd08ba2
Umang A <umanga.eng@gmail.com>
2022-05-20 11:42:45 -0400

    fix: extra nesting removed

--- src/components/navigation/Footer.js
+++ src/components/navigation/Footer.js
@@ -2,15 +2,13 @@ import React from "react";
 import Logo from "../../mhl.png";
 function Footer() {
   return (
-    <div className="">
-      <div className="flex p-5 my-5 justify-center w-full border-t bg-white">
-        <div className="">
-          <img src={Logo} alt="MHL" className="h-16 " />
-        </div>
-        <div className="flex flex-col justify-end">
-          <span className="text-center"> MHL Chats developed By Umang A</span>
-          <span className="text-center">Powered by React.js & Firebase</span>
-        </div>
+    <div className=" flex p-5 my-5 justify-center w-full border-t bg-white">
+      <div className="">
+        <img src={Logo} alt="MHL" className="h-16 " />
+      </div>
+      <div className="flex flex-col justify-end">
+        <span className="text-center"> MHL Chats developed By Umang A</span>
+        <span className="text-center">Powered by React.js & Firebase</span>
       </div>
     </div>
   );
