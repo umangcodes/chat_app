1b4ab28ce84929d7a296c0240952a8d70635aab3
Umang A <umanga.eng@gmail.com>
2022-05-20 09:10:05 -0400

    fix: display name for all the messages fixed

--- src/components/cards/Message.js
+++ src/components/cards/Message.js
@@ -3,7 +3,7 @@ import React from "react";
 function Message(props) {
   const { userId, userName, message, messageFrom, user } = props;
   let sent;
-  if (messageFrom === user) {
+  if (messageFrom === userName) {
     sent = true;
   } else {
     sent = false;
@@ -14,7 +14,7 @@ function Message(props) {
         sent ? "bg-lime-100 rounded-full w-50" : "bg-teal-100 rounded-full"
       }
     >
-      <div className="mx-5 pt-2">{userName}</div>
+      <div className="mx-5 pt-2">{messageFrom}</div>
       <li key={userId} className="mx-8 my-2 pb-2 font-thin">
         {message}
       </li>
