d69f552d088e508680f69c2ed1f5371c8e98273d
Umang A <umanga.eng@gmail.com>
2022-05-20 00:39:26 -0400

    BREAKING CHANGE: disabled header

--- src/components/navigation/Header.js
+++ src/components/navigation/Header.js
@@ -4,17 +4,5 @@ import SignIn from "../buttons/SignIn";
 import SignOut from "../buttons/SignOut";
 export default function Header(props) {
   const { isSignedIn } = props;
-  return (
-    <div className="flex p-5 justify-between">
-      <div className="">
-        <img src={Logo} alt="MHL" className="h-16 " />
-      </div>
-      <div className="w-48">
-        <ul className="flex justify-between">
-          <li className="m-auto">Let's Chat MHL!</li>
-          <li className="m-auto">{isSignedIn ? <SignOut /> : <SignIn />}</li>
-        </ul>
-      </div>
-    </div>
-  );
+  return null;
 }
