60cf8751423ff980689130e6dcbc91229be69880
Umang A <umanga.eng@gmail.com>
2022-05-20 00:40:51 -0400

    feat: css props changed on the page to improve user experince.

--- src/App.js
+++ src/App.js
@@ -4,10 +4,13 @@ import { collection, getDocs } from "firebase/firestore";
 import { db, auth } from "./firebase/Firebase.js";
 import { GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
 import Chatroom from "./components/panels/Chatroom";
+import Footer from "./components/navigation/Footer";
 function App() {
   const [user, setUser] = useState(() => auth.currentUser);
   const [initializing, setInitializing] = useState(true);
   const [signedIn, setSignedIn] = useState(false);
+  // const firstName = user.displayName.split(" ").slice(0, -1).join(" ");
+
   const signInWithGoogle = async () => {
     const provider = new GoogleAuthProvider();
     try {
@@ -34,27 +37,39 @@ function App() {
     return unsubscribe;
   }, [auth, db]);
   return (
-    <div className="App">
-      {user ? (
-        <>
-          <div className="flex my-5 justify-between mx-5">
-            <p className="text-lg">
-              Welcome to MHL chats,
-              <span className="italic"> {user.displayName}</span>
-            </p>
+    <div className="h-screen">
+      <div className="h-screen">
+        {user ? (
+          <>
+            <div className="flex my-5 justify-between mx-5">
+              <p className="text-lg">
+                Welcome to MHL chats,
+                <span className="italic">
+                  {``} {user.displayName.split(" ").slice(0, -1).join(" ")}
+                  {/* {firstName.charAt(0).toUpperCase() + firstName.slice(1)} */}
+                </span>
+              </p>
+              <button
+                className=" font-semibold rounded-full px-4 border hover:shadow-sm text-red-900 transition ease-in-out hover:scale-105"
+                onClick={logOut}
+              >
+                Sign Out
+              </button>
+            </div>
+            <Chatroom user={user} db={db} />
+          </>
+        ) : (
+          <div className="h-4/5 flex justify-center align-center">
             <button
-              className="border-red font-semibold rounded-full px-2 py-1 bg-red-100/75 hover:bg-red-100 transition ease-in-out hover:scale-105"
-              onClick={logOut}
+              onClick={signInWithGoogle}
+              className=" hover:scale-105 transition ease-linear hover:font-semibold"
             >
-              Sign Out
+              Login with Google
             </button>
           </div>
-          <Chatroom user={user} db={db} />
-        </>
-      ) : (
-        <button onClick={signInWithGoogle}>Login</button>
-      )}
-      <Header signInState={signedIn} handleSignIn={setSignedIn} />
+        )}
+      </div>
+      <Footer />
     </div>
   );
 }
